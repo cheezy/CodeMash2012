e28b28f7463865562e1dd745ac3b58088818b999
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-01-12 23:35:58 -0500

    more stuff '

--- Guardfile
+++ Guardfile
@@ -1,7 +1,7 @@
 # A sample Guardfile
 # More info at https://github.com/guard/guard#readme
 
-guard 'cucumber' do
+guard 'cucumber', :all_after_pass => false do
   watch(%r{^features/.+\.feature$})
   watch(%r{^features/support/.+$})                      { 'features' }
   watch(%r{^features/step_definitions/(.+)_steps\.rb$}) { 'features' }
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,14 +5,16 @@ require 'page-object/page_factory'
 require 'faker'
 require 'require_all'
 
-#require_all File.dirname(__FILE__) + '/pages'
+=begin
+require_all File.dirname(__FILE__) + '/pages'
 
-#PageObject::PageFactory.routes = {
-#  :default => [[HomePage, :adopt],
-#               [DetailsPage, :adopt_me],
-#               [ShoppingCartPage, :complete_adoption],
-#               [CheckoutPage, :complete_order]],
-#}
+PageObject::PageFactory.routes = {
+  :default => [[HomePage, :adopt],
+               [DetailsPage, :adopt_me],
+               [ShoppingCartPage, :complete_adoption],
+               [CheckoutPage, :checkout]],
+}
+=end
 
 World(PageObject::PageFactory)
 
